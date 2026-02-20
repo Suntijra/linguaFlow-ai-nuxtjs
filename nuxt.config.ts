ae2c740a5e920e896255f2dd25b1b76a3e239cda
Sunti <52416033+Suntijra@users.noreply.github.com>
2026-02-20 13:06:49 +0700

    github page v1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,11 +14,16 @@ export default defineNuxtConfig({
     ],
   },
   app: {
+    baseURL: process.env.NUXT_APP_BASE_URL || '/',
     head: {
       link: [
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap' }
       ]
     }
+  },
+  ssr: false,
+  nitro: {
+    preset: 'github-pages'
   }
 })
